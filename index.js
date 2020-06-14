d53096a8ac4afc77f760a0279ffae9dd7967ba42
lbirts <birtslauren@gmail.com>
2020-06-14 12:11:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(arr, func) {
+    // return func(arr)
+    let newarr = []
+    for (let i = 0; i < arr.length; i++) {
+        newarr.push(func(arr[i]));
+    }
+    return newarr
+}
+
+function reduce(arr, func, start) {
+    // return func(arr)
+    let value = (start) ? start : arr[0]
+    let i = (start) ? 0 : 1
+    for (; i < arr.length; i++) {
+        value = func(arr[i], value);
+    }
+    return value
+}
